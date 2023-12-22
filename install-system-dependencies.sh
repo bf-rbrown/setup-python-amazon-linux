06f41cb91d1a7ee41fec4c1212c82086245831be
Richard Brown <rbrown@bishopfox.com>
2023-12-21 22:38:29 -0600

    testing debian

--- install-system-dependencies.sh
+++ install-system-dependencies.sh
@@ -4,14 +4,21 @@ set -euo pipefail
 
 # Reference https://stackoverflow.com/a/73208851/3316017
 
-sudo yum update -y
-
 desired_openssl_package="openssl-devel"
-if yum info "openssl11-devel" &>/dev/null; then
-  desired_openssl_package="openssl11-devel"
-fi
 
-sudo yum groupinstall -y "Development Tools"
-sudo yum install -y "${desired_openssl_package}" zlib-devel bzip2 bzip2-devel readline-devel libffi-devel \
-  ncurses-devel sqlite sqlite-devel gdbm-devel tk-devel xz-devel \
-  tar gzip wget
+IDLIKE=$(cat /etc/os-release | grep ID_LIKE | awk -F '=' '{print $2}')
+if [[ ${IDLIKE} == "debian" ]]; then
+  sudo apt-get update -y
+
+else
+  sudo yum update -y
+
+  if yum info "openssl11-devel" &>/dev/null; then
+    desired_openssl_package="openssl11-devel"
+  fi
+
+  sudo yum groupinstall -y "Development Tools"
+  sudo yum install -y "${desired_openssl_package}" zlib-devel bzip2 bzip2-devel readline-devel libffi-devel \
+    ncurses-devel sqlite sqlite-devel gdbm-devel tk-devel xz-devel \
+    tar gzip wget
+fi
